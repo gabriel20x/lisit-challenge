e79fc4c3bd82c10daa6d45b588472b59e35d620d
gabriel20x <gabrielgarciatorresm3@gmail.com>
2023-10-26 06:10:33 -0500

    Add Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
-/** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {
         colors: {
-            "dark-space" : '#120D11'
+            "dark-space" : '#120D11',
+            "blue-space" : '#202133',
+            "light-space" : '#667090',
+        },
+        fontFamily: {
+          sjRegular: 'Star Jedi Regular',
+          sjHollow: 'Star Jedi Hollow',
+          sjOutline: 'Star Jedi Outline',
         }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ]
 }
 
